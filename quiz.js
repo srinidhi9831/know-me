bc273a9854966dbeccb2176292e2baafae2e56f1
srinidhi9831 <srinidhi9831@gmail.com>
2021-10-14 20:06:37 +0530

    feat: added more quiz

--- quiz.js
+++ quiz.js
@@ -1,11 +1,12 @@
 var readlinesync = require("readline-sync");
 var score=0;
-var userName = readlinesync.question("what's your name");
+var userName = readlinesync.question("what's your name\n");
 console.log("welcome "+userName);
 
+
 function play(question,answer){
-  var ans= readlinesync.question(question);
-  if(answer===ans){
+  var ans= readlinesync.question(`${question}\n`);
+  if(answer.toUpperCase()===ans.toUpperCase()){
     console.log("you are right");
     score=score+1;
   }
@@ -19,5 +20,11 @@ function play(question,answer){
 
 play("whats my name","srinidhi");
 play("where do i live ?","bangalore");
-play("where do i study","MSRIT")
+play("where do i study","MSRIT");
+play("can you tell me my hometown","mysore");
+play("whats my age","23");
+play("whats my favourite color?","green");
+play("whats my favourite breakfast","dosa");
+play("My favourite superhero would be?","deadpool");
 console.log("thank you for participating in the quiz");
+console.log("your score:",score);
